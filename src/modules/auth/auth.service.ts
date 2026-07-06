eec79b4ecd9d3eeafe51c787647bdfb90c5121bd
Md Kawsar Ali <mdkawsarali346@.com>
2026-07-06 21:54:34 +0600

    get access token successfully

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import bcrypt from "bcryptjs";
 import prisma from "../../lib/prisma";
 import { TLoginUser } from "./auth.interface";
+import { SignOptions } from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
 const loginUser = async (payload: TLoginUser) => {
   const { email, password } = payload;
@@ -17,9 +20,31 @@ const loginUser = async (payload: TLoginUser) => {
     throw new Error("Password is incorrect");
   }
 
-  const { password: _, ...userWithoutPassword } = user;
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
 
-  return userWithoutPassword;
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as SignOptions["expiresIn"],
+  );
+
+  const userInfo = await prisma.user.findUniqueOrThrow({
+    where: {
+      id: user.id,
+    },
+    omit: {
+      password: true,
+    },
+  });
+  return {
+    accessToken,
+    user: userInfo,
+  };
 };
 
 export const authService = {
