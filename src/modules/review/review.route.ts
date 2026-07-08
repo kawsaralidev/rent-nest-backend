00848a46d2b9e27f7f4110ea743d9f46b5bc1d48
Md Kawsar Ali <mdkawsarali346@.com>
2026-07-08 17:23:21 +0600

    payment system implement done

--- src/modules/review/review.route.ts
+++ src/modules/review/review.route.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import { Role } from "../../../generated/prisma/enums";
-import { reviewController } from "./review.controller";
-import { auth } from "../../middleware/auth";
+// import express from "express";
+// import { Role } from "../../../generated/prisma/enums";
+// import { reviewController } from "./review.controller";
+// import { auth } from "../../middleware/auth";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/", auth(Role.TENANT), reviewController.createReview);
+// router.post("/", auth(Role.TENANT), reviewController.createReview);
 
-export const reviewRoutes = router;
+// export const reviewRoutes = router;
